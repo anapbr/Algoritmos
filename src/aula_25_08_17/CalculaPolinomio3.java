77bd04b80ad2ab0e7eea60014dce12fd85ebd72d
Ana Souza <aninhasouza@gmail.com>
2017-11-08 12:17:10 -0200

    Terceira Solução do Calcula Polinômio

---
+++ src/aula_25_08_17/CalculaPolinomio3.java
@@ -0,0 +1,45 @@
+package aula_25_08_17;
+
+public class CalculaPolinomio3 {
+
+	public static void main(String[] args) {
+
+		float[] A = {2, 3, 1};
+		
+		System.out.println(calcularPolinomio(A, 2));
+		
+	}
+
+	public static float calcularPolinomio(float[] A, float x) {
+
+		float P;
+		float PLinha;
+		int n = A.length;
+		
+		// Quando restar apenas o último coeficiente, 
+		// APENAS RETORNA (não é necessário multiplicar - grau de x = 0)		
+		if (n==1) {
+			P = A[0];
+		} else {
+		
+			// Elimina recursivamente o primeiro coeficiente da matriz 
+			// até que reste apenas um
+			float[] ALinha = new float[n-1];
+			
+			//Eliminando o último termo do Array (o de menor grau)
+			for (int i = 0; i < ALinha.length; i++) {
+				ALinha[i] = A[i];
+			}		
+			
+			//System.out.println();
+			
+			PLinha = calcularPolinomio(ALinha, x);
+			
+			System.out.println("n=" + n + " PLinha=" + PLinha + " A[0]=" + A[0]);
+			
+			P = x * PLinha + A[n-1];
+		}
+		return P;
+	}
+
+}
