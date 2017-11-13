3499d6faaae8f396a82cb825dd75e6ad698aae49
Ana Souza <aninhasouza@gmail.com>
2017-11-13 09:18:34 -0200

    Comentários

--- src/aula_17_09_15/HeapSort.java
+++ src/aula_17_09_15/HeapSort.java
@@ -2,6 +2,27 @@ package aula_17_09_15;
 
 public class HeapSort {
 
+	/*
+	 * Conceitos:
+	 * heap: árvore binária ordenada
+	 * max heap: heap onde nós pais são maiores que os respectivos nós filhos
+	 * 
+	 * Passo a passo:
+	 * 
+	 * 1. Cria a max heap
+	 * 2. Remove o maior elemento
+	 * 3. Coloca este elemento em parte ordenada
+	 * 
+	 * Quando o maior elemento é removido da heap, a mesma fica "desequilibra"
+	 * 
+	 * Como a max heap já havia sido previamente criada, é necessário realizar pequenos ajustes
+	 * 
+	 * Os ajustes serão realizados através do heapify
+	 * Agora que voltamos a ter uma max heap, repetimos os processo.
+	 * 
+	 * Até que todos os elementos tenham sido marcados como ordenados.
+	 * */
+	
 	public static void main(String[] args) {
 		
 		int A[] = { 99, 35, 20, 25, 40, 44, 55, 38, 10, 65, 50, 12 };
@@ -20,15 +41,8 @@ public class HeapSort {
 
 	}
 
-	private static void imprimeVetor (int[] A) {
-		int j = 0;
-		while (j < A.length) {
-			System.out.print(A[j++] + " ");
-		}
-	}
-	
     private static void heapSort(int A[], int n) {
-        maxHeap(A, n);                      
+        buildMaxHeap(A, n);                      
         int m = n + 1;                          
         for (int i = n; i >= 0; i--) {          
             troca(A, 0, i);                     
@@ -37,7 +51,8 @@ public class HeapSort {
         }
     }
 	
-	public static void maxHeap(int[] A, int n) {
+    // Cria a Max Heap a partir de um array não ordenado
+	public static void buildMaxHeap(int[] A, int n) {
 
 		// OBSERVAÇÃO: i = n/2; i receberá o piso da divisão
 		for (int i = n/2; i >= 0; i--) {
@@ -45,21 +60,22 @@ public class HeapSort {
 		}
 	}
 	
+	// Similar ao buildMaxHeap, porém assume que partes do array já estão ordenadas
 	public static void maxHeapify(int[] A, int n, int i) {
 		
 		// Filhos sempre está nas posições 2*i e 2*i + 1
-		int e = 2 * i;
-		int d = 2 * i + 1;
+		int esquerda = 2 * i;
+		int direita = 2 * i + 1;
 		int maior;
 
-		if (e < n && A[e] > A[i]) {
-			maior = e;
+		if (esquerda < n && A[esquerda] > A[i]) {
+			maior = esquerda;
 		} else {
 			maior = i;
 		}
 
-		if (d < n && A[d] > A[maior]) {
-			maior = d;
+		if (direita < n && A[direita] > A[maior]) {
+			maior = direita;
 		}
 
 		if (maior != i) {
@@ -77,4 +93,12 @@ public class HeapSort {
 		A[j] = auxAI;
 	}
 
+	/* Método auxiliar */
+	private static void imprimeVetor (int[] A) {
+		int j = 0;
+		while (j < A.length) {
+			System.out.print(A[j++] + " ");
+		}
+	}
+	
 }
